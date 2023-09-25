425d340f6e6a6852ea9e2b077240d008bf9d734c
Mustafa Akur <106137913+mustafasrepo@users.noreply.github.com>
2023-09-25 17:43:02 +0300

    Adds a new method to construct window function for the given input

--- datafusion/core/src/physical_optimizer/enforce_distribution.rs
+++ datafusion/core/src/physical_optimizer/enforce_distribution.rs
@@ -26,11 +26,12 @@ use std::fmt::Formatter;
 use std::sync::Arc;
 
 use crate::config::ConfigOptions;
-use crate::error::Result;
+use crate::error::{DataFusionError, Result};
 use crate::physical_optimizer::utils::{
     add_sort_above, get_children_exectrees, is_coalesce_partitions, is_repartition,
     is_sort_preserving_merge, ExecTree,
 };
+use crate::datasource::physical_plan::{CsvExec, ParquetExec};
 use crate::physical_optimizer::PhysicalOptimizerRule;
 use crate::physical_plan::aggregates::{AggregateExec, AggregateMode, PhysicalGroupBy};
 use crate::physical_plan::coalesce_partitions::CoalescePartitionsExec;
